d324e3ebcd37bee3e4116ce05bd98ca39e84a9cd
tricao1212 <tri.cao.cit20@eiu.edu.vn>
2024-03-20 22:36:13 +0700

    ranking page

--- frontend/src/pages/user/ranking/Ranking.js
+++ frontend/src/pages/user/ranking/Ranking.js
@@ -8,10 +8,12 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
+import { UserAuth } from "../../../context/AuthContext";
 
 const Ranking = () => {
   const [allUser, setAllUser] = useState([]);
+  const { user } = UserAuth();
   const fetchData = async () => {
     await axios.get("http://34.136.63.21/api/auth").then((response) => {
       setAllUser(response.data.sort((a, b) => b.point - a.point));
@@ -21,20 +23,50 @@ const Ranking = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(allUser);
+
+  const getCurrentTop = () => {
+    for (let x = 0; x < allUser.length; x++) {
+      if (allUser[x].uid === user.uid) {
+        x = x + 1;
+        return x;
+      }
+    }
+    return "";
+  };
+
+  const currentTop = getCurrentTop();
 
   return (
     <>
       <div className={style.rankTable}>
-        <h2>Ranking table</h2>
+        <h2>Your current rank</h2>
         <TableContainer component={Paper}>
           <Table>
+            <TableBody>
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {currentTop}
+                </TableCell>
+                <TableCell>
+                  <Avatar src={user.avatar} />
+                </TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.point}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <h2>Leader Board</h2>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell>Top</TableCell>
-                <TableCell >Avatar</TableCell>
-                <TableCell >Name</TableCell>
-                <TableCell >Total point</TableCell>
+                <TableCell>Avatar</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Total point</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -46,9 +78,11 @@ const Ranking = () => {
                   <TableCell component="th" scope="row">
                     {index + 1}
                   </TableCell>
-                  <TableCell><Avatar src={row.avatar} /></TableCell>
+                  <TableCell>
+                    <Avatar src={row.avatar} />
+                  </TableCell>
                   <TableCell>{row.name}</TableCell>
-                  <TableCell >{row.point}</TableCell>
+                  <TableCell>{row.point}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
--- frontend/src/pages/user/study/Lessons.js
+++ frontend/src/pages/user/study/Lessons.js
@@ -72,7 +72,6 @@ const Lessons = () => {
             </Card.Body>
           </Card>
         )})}
-          
       </div>
     </>
   );
